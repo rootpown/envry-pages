851e2c67c92ca4dc2a46c8eb095893904974040d
rootpown <rootpown@gmail.com>
2024-12-13 11:01:47 +0400

    fix

--- final-round/src/index.js
+++ final-round/src/index.js
@@ -1,5 +1,15 @@
-const burger = document.getElementById('burger');
+const hideElements = document.querySelector('.hideElements');
+const elementBurger = document.getElementById('burger');
+const showContentAside = document.querySelector('.aisde-container');
 
-burger.addEventListener('click', () => {
-  console.log('hi');
+showContentAside.style.display = 'none';
+
+// Обработчик для кнопки "показать"
+elementBurger.addEventListener('click', () => {
+  showContentAside.style.display = 'block';
+});
+
+// Обработчик для кнопки "скрыть"
+hideElements.addEventListener('click', () => {
+  showContentAside.style.display = 'none';
 });
