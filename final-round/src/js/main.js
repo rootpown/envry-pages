b1b96899cb481136857c0372d159075c25e3c418
rootpown <rootpown@gmail.com>
2024-12-23 14:21:01 +0400

    0.1

--- final-round/src/js/main.js
+++ final-round/src/js/main.js
@@ -3,19 +3,18 @@ import '../scss/main.scss';
 import '../index.html';
 import '../index.js';
 import Swiper from 'swiper/bundle';
-
 import 'swiper/css/bundle';
 
 let swipers = [];
-let statusWidth = window.innerWidth > 768;
+let statusWidth = window.innerWidth > 767;
 
 function initSwiper(className, breakpoints) {
   const elements = document.querySelectorAll(className);
   elements.forEach((element, index) => {
     if (statusWidth) {
       if (swipers[index]) {
-        swipers[index].destroy(true, true); // Уничтожаем Swiper, если он существует
-        swipers[index] = null; // Удаляем из массива
+        swipers[index].destroy(true, true);
+        swipers[index] = null;
       }
     } else {
       if (!swipers[index]) {
@@ -24,15 +23,21 @@ function initSwiper(className, breakpoints) {
           slidesPerView: 1,
           spaceBetween: 30,
           updateOnWindowResize: true,
+          pagination: {
+            el: '.swiper-pagination',
+          },
         });
         swipers[index] = swiper;
       }
     }
+    console.log(swipers);
   });
 }
+
 const breakpoints = {
   500: {
     slidesPerView: 2,
+    spaceBetween: 40,
   },
   768: {
     slidesPerView: 3,
@@ -40,7 +45,7 @@ const breakpoints = {
 };
 
 window.addEventListener('resize', () => {
-  statusWidth = window.innerWidth > 768;
+  statusWidth = window.innerWidth > 767;
   initSwiper('.swiper', breakpoints);
   swipers.forEach((swiper) => {
     if (swiper) {
@@ -51,3 +56,29 @@ window.addEventListener('resize', () => {
 });
 
 initSwiper('.swiper', breakpoints);
+
+function initContent() {
+  const buttons = document.querySelectorAll('.tqs_block_button');
+  const elementBlocks = document.querySelectorAll('.tqs_block_style');
+
+  elementBlocks.forEach((block, index) => {
+    if (index >= elementBlocks.length - 4) {
+      block.classList.add('hidden');
+    }
+  });
+
+  buttons.forEach((button, buttonIndex) => {
+    button.addEventListener('click', () => {
+      const swiperContainer = button
+        .closest('.tqs_block')
+        .querySelectorAll('.tqs_block_style');
+      swiperContainer.forEach((block, index) => {
+        if (index >= swiperContainer.length - 4) {
+          block.classList.toggle('hidden');
+        }
+      });
+    });
+  });
+}
+
+initContent();
